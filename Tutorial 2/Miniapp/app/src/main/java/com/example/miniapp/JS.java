348694a4b0aaa2b1893afff8cfcb1797f6f315b9
BugsCleaners <91881471+BugsCleaners@users.noreply.github.com>
2022-05-09 19:11:40 +0300

    miniapp

---
+++ Tutorial 2/Miniapp/app/src/main/java/com/example/miniapp/JS.java
@@ -0,0 +1,22 @@
+package com.example.miniapp;
+
+import android.content.Context;
+import android.webkit.JavascriptInterface;
+import android.widget.Toast;
+
+public class JS
+{
+     private Context context;
+
+     public JS(Context ctx)
+     {
+         this.context = ctx;
+     }
+     @JavascriptInterface
+    public void Getlink(String link)
+    {
+        Toast.makeText(context, link, Toast.LENGTH_SHORT).show();
+    }
+
+
+}
